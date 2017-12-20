9a42e1f0fb0a764564e14f8092b47d2237179058
dongxi346 <1105894953@qq.com>
2017-12-20 23:03:53 +0800

    创建用户账户，书上的路径是个坑，已解决！

--- leaning_log/settings.py
+++ leaning_log/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
 
     # 我的应用程序
     'leaning_logs',
+    'users',
 ]
 
 MIDDLEWARE = [
@@ -121,3 +122,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+# My settings
+LOGIN_URL = '/users/login/'
--- leaning_log/urls.py
+++ leaning_log/urls.py
@@ -18,6 +18,7 @@ from django.urls import include, path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('users/', include('users.urls',namespace='users')),
     path(r'', include('leaning_logs.urls',namespace='leaning_logs')),
 ]
 
