dedf92972fea95d8a4296790b06ce36aa8400102
dongxi346 <1105894953@qq.com>
2017-12-22 22:58:42 +0800

    添加注释

--- leaning_logs/models.py
+++ leaning_logs/models.py
@@ -12,7 +12,7 @@ class Topic(models.Model):
         return self.text
 
 class Entry(models.Model):
-    """Something specific learned about a topic."""
+    """用户发表的文章"""
     topic = models.ForeignKey(Topic, "on_delete=models.CASCADE")
     text = models.TextField()
     date_added = models.DateTimeField(auto_now_add = True)
@@ -21,5 +21,5 @@ class Entry(models.Model):
         verbose_name_plural = 'entries'
 
     def __str__(self):
-        """Return a string representation of the model."""
+        """返回模型的字符串表示"""
         return self.text[:50] + "..."
