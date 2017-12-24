c0ea04efa331758117c8f8cab3f473ed7ebc9f9d
dongxi346 <1105894953@qq.com>
2017-12-24 12:55:28 +0800

    添加注释笔记

--- leaning_logs/admin.py
+++ leaning_logs/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 
 from leaning_logs.models import Topic, Entry
-
+# 注册表单
 admin.site.register(Topic)
 admin.site.register(Entry)
 
--- leaning_logs/models.py
+++ leaning_logs/models.py
@@ -5,6 +5,7 @@ class Topic(models.Model):
     """用户要学习的主题."""
     text = models.CharField(max_length=200)
     date_added = models.DateTimeField(auto_now_add=True)
+    # 外键是用户，可以通过这个外键确定这个主题属于哪个用户
     owner = models.ForeignKey(User, "on_delete=models.CASCADE")
 
     def __str__(self):
@@ -13,6 +14,7 @@ class Topic(models.Model):
 
 class Entry(models.Model):
     """用户发表的文章"""
+    # 外键是主题，可以通过这个外键确定这个文章属于哪个主题
     topic = models.ForeignKey(Topic, "on_delete=models.CASCADE")
     text = models.TextField()
     date_added = models.DateTimeField(auto_now_add = True)
